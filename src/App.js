8df73c4f84ac9d2e96b76b4cbc0ef6c13bc5fb13
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-12-30 19:03:49 +0800

    blog
    blog

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import BlogIndex from "pages/BlogIndex";
 // import Blog from "Blog.js";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import useFetch from "hooks/useFetch";
 
 export default function App() {
-
+  const { loading, error, data } = useFetch("http://localhost:1337/api/blogs?populate=*");
+  if (loading) return <p>loading</p>;
+  if (error) return <p>Error</p>;
 
   return (
     <Router>
